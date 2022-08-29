d527c8e600f05e4fd31b7f8afa41d6cb7169993b
hadrylouis <hadry.louis@gmail.com>
2022-08-29 15:40:03 +0200

    [Done] particularity select

--- src/components/settings/Cooktype.js
+++ src/components/settings/Cooktype.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Select from 'react-select'
-import makeAnimated from 'react-select/animated';
+import makeAnimated from 'react-select/animated'
 import apiConfig from "../../config/api.config"
 
 const animatedComponents = makeAnimated();
--- src/components/settings/Particularities.js
+++ src/components/settings/Particularities.js
@@ -1,27 +1,56 @@
+import React from "react"
 import Select from 'react-select'
-import makeAnimated from 'react-select/animated';
-
-const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' },
-    { value: 'lemon', label: 'Lemon2' }
-  ]
+import makeAnimated from 'react-select/animated'
+import apiConfig from "../../config/api.config"
 
 const animatedComponents = makeAnimated();
 
-function Particularities() {
-    return (
-        <>
-            <h2>Particularities</h2>
-            <Select
-            closeMenuOnSelect={false}
-            components={animatedComponents}
-            isMulti
-            options={options}
-            />
-        </>
-    )
+class Particularities extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            particularity: []
+        }
+        this.fetchParticularity = this.fetchParticularity.bind(this)
+    }
+
+    componentDidMount() {
+        this.setState({
+            particularity: this.fetchParticularity(),
+        })
+    }
+
+    async fetchParticularity() {
+        fetch(apiConfig.url + '/v0/recipe/getParticularity')
+            .then(response => response.json())
+            .then(data => {
+                const particularity = []
+                for (const [value, label] of Object.entries(data.Particularity)) { 
+                    particularity.push({
+                        'value': value,
+                        'label': label
+                    })
+                }
+                this.setState({particularity: particularity})
+            })
+            .catch(error => this.props.onError())
+    }
+
+    render() {
+        return (
+            <>
+                <h2>Particularities</h2>
+                <Select
+                closeMenuOnSelect={false}
+                components={animatedComponents}
+                isMulti
+                options={this.state.particularity}
+                />
+            </>
+        )
+    }
+
 }
 
 export default Particularities
