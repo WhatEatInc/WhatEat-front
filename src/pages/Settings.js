e42fe36f71347fedf1b3f539264bba1c88d7878e
hadrylouis <hadry.louis@gmail.com>
2022-08-30 09:11:20 +0200

    [WIP] get user settings

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -6,6 +6,7 @@ import Cooktype from '../components/settings/Cooktype'
 import Duration from '../components/settings/Duration'
 import Health from '../components/settings/Health'
 import Error from '../components/error/Error'
+import apiConfig from "../config/api.config"
 
 class Settings extends React.Component {
 
@@ -15,6 +16,19 @@ class Settings extends React.Component {
             hasError: false
         }
         this.onError = this.onError.bind(this)
+    }
+
+    componentDidMount() {
+        this.fetchPreferences()
+    }
+
+    async fetchPreferences() {
+        fetch(apiConfig.url + '/v0/recipe/getCookType')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+            })
+            .catch(error => this.props.onError())
     }
 
     onError() {
@@ -34,10 +48,10 @@ class Settings extends React.Component {
                 <>
                 <h1>Settings</h1>
                 <User />
-                <Allergens onError={this.onError}/>
+                <Allergens onError={this.onError} />
                 <Particularities onError={this.onError} />
                 <Cooktype onError={this.onError} />
-                <Duration onError={this.onError}/>
+                <Duration onError={this.onError} />
                 <Health/>
                 </>
         )}
