99de5efd5f60d3d42848b10216648bd50f5bbe89
Lindwing <rui.lopesgouveia@heig-vd.ch>
2022-08-30 16:27:30 +0200

    [WIP] adding the base of the login

---
+++ src/pages/Login.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import apiConfig from "../config/api.config"
+
+class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      pass: "",
+      token: null,
+      data: null
+    };
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePass = this.handleChangePass.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleChangeEmail(event)   {    this.setState({email: event.target.value});  }
+  handleChangePass(event)    {    this.setState({pass: event.target.value});   }
+
+  handleSubmit(event) {
+
+        // Simple POST request with a JSON body using fetch
+        const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 
+            email: this.state.email,
+            password: this.state.pass,
+        })
+        }
+
+        if(this.state.pass === this.state.confirm){
+        fetch(apiConfig.url + "/v0/user/login", requestOptions)
+            .then(response => {
+
+            if(response.ok){
+                this.state.data = response.json()
+                console.log(this.state.data)
+            }
+            })
+
+        event.preventDefault();
+        }
+    } 
+
+
+    render() {   
+        return(
+          <form onSubmit={this.handleSubmit}>
+              <label>
+                Email:
+                <input type="text" name="email" placeholder="put your email here" value={this.state.email} onChange={this.handleChangeEmail} required/>
+              </label>
+              <label>
+                Password:
+                <input type="text" value={this.state.pass} onChange={this.handleChangePass} required/>
+              </label>
+                <input type="submit" value="Submit" />
+              <div id="err"></div>
+          </form>
+        )
+    }
+}
+
+
+export default Login 
