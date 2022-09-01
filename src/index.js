13f2902ab1549d9a230bff4c3dd0bf91e88dba04
Damiano <35708577+damianomondaini@users.noreply.github.com>
2022-09-01 09:11:48 +0200

    Revert "Fb settings"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './components/app/App'
 import './css/app.css'
-import "react-toggle/style.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
--- src/pages/Today.js
+++ src/pages/Today.js
@@ -1,3 +1,4 @@
+
 import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faDice } from "@fortawesome/free-solid-svg-icons"
